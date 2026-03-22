c1fa8833d2c8b86302a09a5b5e5de4a460d68eeb
vainqueur kalema <vainqueurkalema035@gmail.com>
2026-03-22 12:06:32 +0100

    deploy

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ class Config :
     SECRET_KEY = os.getenv('SECRET_KEY','00Kalema')
     SQLALCHEMY_DATABASE_URI =os.getenv('DATABASE_URL','sqlite:///Finances.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    PORT=5000
