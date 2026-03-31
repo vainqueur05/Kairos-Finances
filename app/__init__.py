ea78327e55097d5639fdac095e65305c85f55b5f
vainqueur kalema <vainqueurkalema035@gmail.com>
2026-03-31 18:22:58 +0200

    Mise à jour interface / UI Luxe

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-
+from datetime import datetime
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -21,6 +21,10 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
+
+    @app.context_processor
+    def inject_now():
+        return {'now': datetime.now()}
 
     from app.models import User
     @login_manager.user_loader
